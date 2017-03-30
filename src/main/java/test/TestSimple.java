b22a7cf8d158f73961dc73c9bd5d65a203fd273c
gongchunru <gongchunru@163.com>
2017-03-30 15:44:31 +0800

    ..

--- src/main/java/test/TestSimple.java
+++ src/main/java/test/TestSimple.java
@@ -1,14 +1,24 @@
 package test;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by gongchunru on 2016/9/19.
  */
 public class TestSimple {
     public static void main(String[] args) {
-        int[] ints = new int[]{1,2,3};
-        System.out.println(ints.toString());
-        System.out.println(Arrays.toString(ints));
+        Map<String, Object> map = new HashMap<>();
+        map.put("hello", "a");
+        map.put("world", "b");
+
+        for (Map.Entry<String,Object> entry:map.entrySet()){
+            System.out.println(entry.getKey()+", "+ entry.getValue());
+        }
+
+        Set<String> strings = map.keySet();
+
     }
 }
