a3def949632c5bf179aff6a57334ce81e7219063
gongchunru <gongchunru@163.com>
2017-08-27 23:21:43 +0800

    modify some

--- src/main/java/com/java/improve/callBack/Testb.java
+++ src/main/java/com/java/improve/callBack/Testb.java
@@ -6,16 +6,24 @@ package com.java.improve.callBack;
  * @date 16/9/15 17:14
  */
 public class Testb implements ICall {
+
+    protected static String aa = "这是一个字符串";
+
     @Override
-    public void method() {
-        System.out.println("回调");
+    public void method(String a) {
+        aa = a;
+        System.out.println("  中间值  "+aa);
     }
 
 
     public static void main(String[] args) {
+
         Caller caller = new Caller();
-        caller.setcallFun(new Testb());
-        caller.call();
+        String a = caller.setcallFun(new Testb());
+        System.out.println("" +
+                "输出步骤中间值" + aa);
+        System.out.println("输出最终值： " + a);
+        caller.call("这是一个值");
 
     }
 
