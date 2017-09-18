c7f7b4f4c09fa143a8c22e96908534d6fa488cbc
gongchunru <gongchunru@163.com>
2017-09-18 18:39:09 +0800

    '..'

--- src/main/java/test/TestSimple.java
+++ src/main/java/test/TestSimple.java
@@ -1,10 +1,12 @@
 package test;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by gongchunru on 2016/9/19.
@@ -21,23 +23,38 @@ public class TestSimple {
 //
 //        Set<String> strings = map.keySet();
 
+        BigDecimal bigDecimal = new BigDecimal(1.23-1.23);
+        System.out.println(bigDecimal);
+        System.out.println(bigDecimal.equals(BigDecimal.ZERO));
 
-        ConcurrentHashMap map = new ConcurrentHashMap();
-        map.put("1","a");
-        map.put("2","b");
+        AtomicInteger i = new AtomicInteger(1);
+        AtomicInteger sum = new AtomicInteger(0);
+//        for (int j = 0; j < 10; j++) {
+//            i.incrementAndGet();
+//        }
 
-
-        System.out.println(map.get("1").toString());
-
-        map.putIfAbsent("1","b");
-        map.put("3","c");
-
-
-        System.out.println(map.get("1").toString());
-        map.put("1","d");
-        System.out.println(map.get("1").toString());
+        sum.incrementAndGet();
+        System.out.println(i.intValue() == sum.intValue());
 
 
-        System.out.println(map.get("3").toString());
+
+//
+//        ConcurrentHashMap map = new ConcurrentHashMap();
+//        map.put("1","a");
+//        map.put("2","b");
+//
+//
+//        System.out.println(map.get("1").toString());
+//
+//        map.putIfAbsent("1","b");
+//        map.put("3","c");
+//
+//
+//        System.out.println(map.get("1").toString());
+//        map.put("1","d");
+//        System.out.println(map.get("1").toString());
+//
+//
+//        System.out.println(map.get("3").toString());
     }
 }
