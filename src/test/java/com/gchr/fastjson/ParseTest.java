583b3b4cea3b0c0aea7d54d69a9fc547d6711bfc
gongchunru <gongchunru@163.com>
2017-11-01 15:29:51 +0800

    ,

---
+++ src/test/java/com/gchr/fastjson/ParseTest.java
@@ -0,0 +1,17 @@
+package com.gchr.fastjson;
+
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Test;
+
+/**
+ * Created by gongchunru
+ * Date：2017/11/1.
+ * Time：15:09
+ */
+public class ParseTest {
+
+    @Test
+    public void test1(){
+    }
+
+}
