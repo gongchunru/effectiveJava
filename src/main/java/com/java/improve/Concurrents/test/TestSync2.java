96fc7bb7f9af76dd88d1eb7c66531dae9c4472c5
gongchunru <gongchunru@163.com>
2018-03-03 19:00:06 +0800

    test

--- src/main/java/com/java/improve/Concurrents/test/TestSync2.java
+++ src/main/java/com/java/improve/Concurrents/test/TestSync2.java
@@ -1,5 +1,8 @@
 package com.java.improve.Concurrents.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author gongchunru
  * @Package com.java.improve.Concurrents.test
@@ -22,13 +25,28 @@ public class TestSync2 implements Runnable{
     }
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("main:"+Thread.currentThread().getName());
-        TestSync2 tt = new TestSync2();
-        Thread t = new Thread(tt);//1
-        t.start();//2
+//        System.out.println("main:"+Thread.currentThread().getName());
+//        TestSync2 tt = new TestSync2();
+//        Thread t = new Thread(tt);//1
+//        t.start();//2
+//
+//        tt.m2();//3
+//        System.out.println("main thread b="+ tt.b);//4
 
-        tt.m2();//3
-        System.out.println("main thread b="+ tt.b);//4
+
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+
+        for (String s : list) {
+            if ("2".equals(s)){
+                list.remove(s);
+            }
+        }
+
+
+
+
     }
 
 
